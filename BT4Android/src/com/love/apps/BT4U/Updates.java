2a3c3375920e518e75d2c3e2114bdfddf93f29f0
jcristy <jcristy@vt.edu>
2012-09-20 21:34:36 -0400

    changed the list of departures to a ListView which will allow adding more options branching off from that in the future.  For now, just added the ability to see 'TripNotes' such as last bus of the night etc.

--- BT4Android/src/com/love/apps/BT4U/Updates.java
+++ BT4Android/src/com/love/apps/BT4U/Updates.java
@@ -94,7 +94,7 @@ public class Updates extends SherlockFragment
 		protected void onPostExecute(String result) {
 			resultsSoFar = resultsSoFar + result;
 			
-			if (result.contains("</rss>"))
+			if (result!=null && result.contains("</rss>"))
 			{
 				
 				try{
